2394fd66ca5fb83d20fe8d775357b096ee115021
barhmate <barhmate.mohamed@gmail.com>
2023-11-09 19:48:23 -0500

    confirmation

---
+++ Confirmation.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Confirmation</title>
+</head>
+<body>
+    <h2>Confirmation</h2>
+    <p>Les données ont été enregistrées avec succès dans la base de données.</p>
+</body>
+</html>
